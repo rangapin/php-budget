fd16d25767a49214d24501093a882c67a582caa9
Richard <rangapin@gmx.com>
2020-03-29 15:24:29 +0200

    init

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Log In</title>
+   </head>
+   <body>
+      <div class="container">
+         <h1>Web Budget</h1>
+         <div class="login">
+            <div class="heading">Log in</div>
+            <div class="body">
+               <form role="form" action="" method="post" id="" name="login">
+                  <fieldset>
+                     <div class="login-form">
+                        <input class="input-form" placeholder="E-mail" name="email" type="email" autofocus="" required="true">
+                     </div>
+                     <div class="login-form">
+                        <input class="input-form" placeholder="Password" name="password" type="password" value="" required="true">
+                     </div>
+                     <div class="checkbox">
+                        <button type="submit" value="login" name="login" class="btn btn-primary">Login</button>
+                        <span><a href="register.php" class="btn btn-primary">Register</a></span>
+                        <br>
+                        <br>
+                        <a href="password.php">Forgot Password?</a>
+                     </div>
+                  </fieldset>
+               </form>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
