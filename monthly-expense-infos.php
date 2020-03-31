9339d80ad34a01c1493daede5c06efa905cd58ba
Richard <rangapin@gmx.com>
2020-03-31 18:31:05 +0200

    added report html files

---
+++ monthly-expense-infos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <title>Monthly Report</title>
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+         </div>
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="panel">
+                  <div class="heading">Monthly Report</div>
+                  <div class="body">
+                     <div class="col-md-12">
+                        <h5>Expense Report</h5>
+                        <hr />
+                        <table id="datatable" class="table">
+                           <thead>
+                              <tr>
+                              <tr>
+                                 <th>Item Number</th>
+                                 <th>Date</th>
+                                 <th>Expense Amount</th>
+                              </tr>
+                              </tr>
+                           </thead>
+                           <tr>
+                              <td></td>
+                              <td></td>
+                              <td></td>
+                           </tr>
+                           <tr>
+                              <th>Total</th>
+                              <td>Total</td>
+                           </tr>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
---
+++ yearly-expense-infos.php
@@ -0,0 +1,49 @@
+Formatted HTML:
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+      <title>Yearly Report</title>
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+         </div>
+         <div class="row">
+            <div class="col-lg-12">
+               <div class="panel">
+                  <div class="panel">Yearly Report</div>
+                  <div class="panel">
+                     <div class="col-md-12">
+                        <h5>Yearly Report</h5>
+                        <hr />
+                        <table id="datatable" class="table">
+                           <thead>
+                              <tr>
+                              <tr>
+                                 <th>Item Number</th>
+                                 <th>Year</th>
+                                 <th>Expense Amount</th>
+                              </tr>
+                              </tr>
+                              <tr>
+                                 <td></td>
+                                 <td></td>
+                                 <td></td>
+                              </tr>
+                              <tr>
+                                 <th>Total</th>
+                                 <td></td>
+                              </tr>
+                        </table>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
